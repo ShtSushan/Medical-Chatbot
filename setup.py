e1a2352e9315f9000b474972a64d35bb8145043b
ShtSushan <shtsushan@gmail.com>
2026-03-30 17:13:04 +0545

    packages installed

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="Medical_chatbot",
+    version="0.1.0",
+    author="Sushan",
+    author_email="shtsushan@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
