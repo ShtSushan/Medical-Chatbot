d51c5721a4f5c4c90f032c2caf5d27a6eac317dd
ShtSushan <shtsushan@gmail.com>
2026-03-31 16:22:49 +0545

    Ready for deployment

--- app.py
+++ app.py
@@ -93,4 +93,4 @@ def chat():
     return jsonify({"response": response["answer"]})
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=8080, debug=True)
+    app.run(host="0.0.0.0", port=7860, debug=True)
